0b3ebff8f186321c7ef3ef21310fff7cb40a9b30
kostyaHrytsyuk <streetman889@gmail.com>
2018-12-10 23:57:43 +0200

    Added saving result file

--- visual.py
+++ visual.py
@@ -9,7 +9,7 @@ def read_input_file(file):
 
 if __name__ == '__main__':
     information = read_input_file("a_example.in")
-    # information = read_input_file("b_short_walk.in")
+    # informa1tion = read_input_file("b_short_walk.in")
     # information = read_input_file("c_going_green.in")
     # information = read_input_file("d_wide_selection.in")
     # information = read_input_file("e_precise_fit.in")
@@ -19,4 +19,17 @@ if __name__ == '__main__':
     new_city.build()
     score = new_city.get_score()
 
-    print(score)
+    res = []
+    res.append(len(new_city.utilities) + len(new_city.utilities))
+
+    for r in new_city.residentials:
+        house = new_city.residentials[r]
+        res.append([house.project_number, house.left_top_corner[0], house.left_top_corner[1]])
+
+    for u in new_city.utilities:
+        res.append([u.project_number, u.left_top_corner[0], u.left_top_corner[1]])
+
+    f = open('submission.txt', 'w')
+    for l in res:
+        f.write(str(l)+'\n')
+
